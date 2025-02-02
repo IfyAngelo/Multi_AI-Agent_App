6a4de67167ca51e5701cbaedd3b3208cfabd3a0d
IfyAngelo <michaelangeloifeanyichukwu@gmail.com>
2025-02-02 14:11:21 +0100

    Added sanitize)_data_tool

---
+++ src/tools/sanitize_data_tool.py
@@ -0,0 +1,19 @@
+from agents.agent_base import AgentBase
+
+class SanitizeDataTool(AgentBase):
+    def __init__(self, max_retries, verbose=True):
+        super().__init__(name="SanitizeDataTool", max_retries=max_retries, verbose=verbose)
+
+    def execute(self, medical_data):
+        messages = [
+            {"role": "system", "content": "You are an AI assistant that sanitizes medical data by removing Prtected Health Information (PHI):"},
+            {
+                "role": "user",
+                "content": (
+                    "Remove all PHI from the following data:\n\n"
+                    f"{medical_data}\n\Sanitized Data:"
+                )
+            }
+        ]
+        sanitized_data = self.call_openai(messages, max_tokens=300)
+        return sanitized_data
